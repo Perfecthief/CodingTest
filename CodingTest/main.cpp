f09d987c3963c01ae1cf2feec7a0dead5964005e
OhGwanYun <adfg518092@gmail.com>
2023-11-06 16:25:33 +0900

    Initial Commit

---
+++ CodingTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodingTest
+//
+//  Created by 오관윤 on 2023/11/06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
